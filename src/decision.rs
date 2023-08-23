94c7a44bfebdb2d114a6ac3aac1a1364850e24de
Alex Colby <alexanderdcolby@gmail.com>
2023-08-23 23:02:36 +0100

    feat: adds decision implementation for graphs

--- src/decision.rs
+++ src/decision.rs
@@ -1,13 +1,35 @@
-use crate::state::State;
+use super::State;
+use std::collections::HashSet;
+use std::hash::Hash;
 
 pub trait Decision<S: State> {
   type Actions: IntoIterator<Item = S::Action>;
-  fn actions(&self, observation: &S::Observation) -> Self::Actions;
+  fn actions(&mut self, observation: &S::Observation) -> Self::Actions;
 }
 
 impl<F: Fn(&S::Observation) -> A, S: State, A: IntoIterator<Item = S::Action>> Decision<S> for F {
   type Actions = A;
-  fn actions(&self, observation: &S::Observation) -> Self::Actions {
+  fn actions(&mut self, observation: &S::Observation) -> Self::Actions {
     self(observation)
+  }
+}
+
+pub struct GraphDecision<S: State, D: Decision<S>> {
+  seen: HashSet<S::Observation>,
+  actions_for: D,
+}
+
+impl<S: State, D: Decision<S>> Decision<S> for GraphDecision<S, D>
+where
+  S::Observation: Hash + Eq + Clone,
+{
+  type Actions = Vec<S::Action>;
+  fn actions(&mut self, observation: &<S as State>::Observation) -> Self::Actions {
+    if self.seen.contains(observation) {
+      return Vec::new();
+    }
+
+    self.seen.insert(observation.clone());
+    self.actions_for.actions(observation).into_iter().collect()
   }
 }
