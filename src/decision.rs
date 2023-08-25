39722fcd85a41500fcc260553849d3041608a441
Alex Colby <alexanderdcolby@gmail.com>
2023-08-25 19:29:42 +0100

    tweak: provides an implementation of decisions   for graph searches (where previously seen observations are not chosen)

--- src/decision.rs
+++ src/decision.rs
@@ -1,22 +1,38 @@
 use super::State;
-use std::collections::HashSet;
+use dashmap::DashSet;
 use std::hash::Hash;
 
 pub trait Decision<S: State> {
   type Actions: IntoIterator<Item = S::Action>;
-  fn actions(&mut self, observation: &S::Observation) -> Self::Actions;
+  fn actions(&self, observation: &S::Observation) -> Self::Actions;
 }
 
 impl<F: Fn(&S::Observation) -> A, S: State, A: IntoIterator<Item = S::Action>> Decision<S> for F {
   type Actions = A;
-  fn actions(&mut self, observation: &S::Observation) -> Self::Actions {
+  fn actions(&self, observation: &S::Observation) -> Self::Actions {
     self(observation)
   }
 }
 
-pub struct GraphDecision<S: State, D: Decision<S>> {
-  seen: HashSet<S::Observation>,
+#[derive(Debug, Clone, Default)]
+pub struct GraphDecision<S: State, D: Decision<S>>
+where
+  S::Observation: Hash + Eq,
+{
+  seen: DashSet<S::Observation>,
   actions_for: D,
+}
+
+impl<S: State, D: Decision<S>> From<D> for GraphDecision<S, D>
+where
+  S::Observation: Hash + Eq,
+{
+  fn from(actions_for: D) -> Self {
+    Self {
+      seen: DashSet::new(),
+      actions_for,
+    }
+  }
 }
 
 impl<S: State, D: Decision<S>> Decision<S> for GraphDecision<S, D>
@@ -24,12 +40,10 @@ where
   S::Observation: Hash + Eq + Clone,
 {
   type Actions = Vec<S::Action>;
-  fn actions(&mut self, observation: &<S as State>::Observation) -> Self::Actions {
-    if self.seen.contains(observation) {
+  fn actions(&self, observation: &<S as State>::Observation) -> Self::Actions {
+    if !self.seen.insert(observation.clone()) {
       return Vec::new();
     }
-
-    self.seen.insert(observation.clone());
     self.actions_for.actions(observation).into_iter().collect()
   }
 }
