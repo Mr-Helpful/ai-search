0c2a89e2d1ed5ec42478c33e3d8eb920343db07a
Alex Colby <alexanderdcolby@gmail.com>
2023-08-26 14:48:34 +0100

    feat: adds `IDFS` search

--- src/search/idfs.rs
+++ src/search/idfs.rs
@@ -1,22 +1,37 @@
-// use super::{Decision, State, DLS};
+use super::{Decision, Search, State, DLS};
 
-// pub struct IDFS<S: State, D> {
-//   start: S,
-//   depth: usize,
-//   search: DLS<S, D>,
-// }
+pub struct IDFS<S: State, D> {
+  start: S,
+  limit: usize,
+  search: DLS<S, D>,
+}
 
-// impl<S: State, D: Decision<S>> Iterator for IDFS<S, D>
-// where
-//   S: Clone,
-// {
-//   type Item = Result<S, S::Error>;
+impl<S: State, D: Decision<S>> Iterator for IDFS<S, D>
+where
+  S: Clone,
+{
+  type Item = Result<S, S::Error>;
 
-//   fn next(&mut self) -> Option<Self::Item> {
-//     let result = self.search.next()?;
-//     if result.is_err() {
-//       self.search.limit += 1;
-//     }
-//     Some(result)
-//   }
-// }
+  fn next(&mut self) -> Option<Self::Item> {
+    loop {
+      let result = self.search.next();
+      if result.is_some() {
+        return result;
+      }
+
+      self.limit += 1;
+      self.search.set_limit(self.start.clone(), self.limit);
+    }
+  }
+}
+
+impl<S: State, D: Decision<S>> Search<S> for IDFS<S, D>
+where
+  S: Clone,
+{
+  fn restart_from(&mut self, start: S) {
+    self.start = start;
+    self.limit = 0;
+    self.search.set_limit(self.start.clone(), self.limit);
+  }
+}
