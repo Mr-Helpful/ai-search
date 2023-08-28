c6ecac989db622a7380798c3d3558f9f9abc5764
Alex Colby <alexanderdcolby@gmail.com>
2023-08-28 21:34:16 +0100

    tweak: splits out `DepthState`

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -60,26 +60,5 @@ where
   }
 }
 
-struct DepthState<S> {
-  state: S,
-  depth: usize,
-}
-
-impl<S: State> State for DepthState<S> {
-  type Error = S::Error;
-  type Observation = (S::Observation, usize);
-  type Action = S::Action;
-  type ObserveError = S::ObserveError;
-  type ResultError = S::ResultError;
-
-  fn observe(&self) -> Result<Self::Observation, Self::ObserveError> {
-    Ok((self.state.observe()?, self.depth))
-  }
-
-  fn result(&self, action: &Self::Action) -> Result<Self, Self::ResultError> {
-    self.state.result(action).map(|state| DepthState {
-      state,
-      depth: self.depth + 1,
-    })
-  }
-}
+mod depth;
+pub use depth::DepthState;
