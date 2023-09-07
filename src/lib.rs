55d7e4a5d4ee490ae2a23416e15456561836fe14
Alex Colby <alexanderdcolby@gmail.com>
2023-09-07 21:59:03 +0100

    feat: adds `prelude` module for easy import

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
+pub mod prelude;
 pub mod search;
 pub mod state;
 pub mod value;
---
+++ src/prelude.rs
@@ -0,0 +1,5 @@
+pub use crate::{
+  search::{Bfs, Dfs, Dls, Greedy, Ids, Uniform},
+  state::{DepthState, GraphState},
+  value::{SearchCost, SearchHeuristic},
+};
