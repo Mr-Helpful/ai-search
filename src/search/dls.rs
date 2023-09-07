92df87d32b6dadeb6e88325354e51844784e39c9
Alex Colby <alexanderdcolby@gmail.com>
2023-09-07 14:27:54 +0100

    tweak: implements `Dls` using `DepthState`

--- src/search/dls.rs
+++ src/search/dls.rs
@@ -1,10 +1,11 @@
 use super::{Decision, Search, State};
+use crate::state::DepthState;
 
 /// A Depth first traversal of possible states, up to a given depth.
 ///
 /// As we give a limit to the depth of the search, we can avoid infinite loops.
 pub struct Dls<S: State, D> {
-  states: Vec<(Result<S, S::Error>, usize)>,
+  states: Vec<Result<DepthState<S>, S::Error>>,
   actions_for: D,
   limit: usize,
 }
@@ -12,7 +13,7 @@ pub struct Dls<S: State, D> {
 impl<S: State, D> Dls<S, D> {
   pub fn new(start: S, actions_for: D, limit: usize) -> Self {
     Self {
-      states: vec![(Ok(start), 0)],
+      states: vec![Ok(start.into())],
       actions_for,
       limit,
     }
@@ -32,36 +33,36 @@ impl<S: State, D: Decision<S>> Iterator for Dls<S, D> {
   type Item = Result<S, S::Error>;
 
   fn next(&mut self) -> Option<Self::Item> {
-    let (result, depth) = self.states.pop()?;
+    let result = self.states.pop()?;
     let state = match result {
       Ok(state) => state,
-      Err(_) => return Some(result),
+      Err(e) => return Some(Err(e)),
     };
 
-    if depth >= self.limit {
-      return Some(Ok(state));
-    }
-
-    let observation = match state.observe() {
+    let (observation, depth) = match state.observe() {
       Ok(observation) => observation,
       Err(e) => return Some(Err(e.into())),
     };
+
+    if depth >= self.limit {
+      return Some(Ok(state.state()));
+    }
 
     let actions = self
       .actions_for
       .actions(&observation)
       .into_iter()
-      .map(|action| (state.result(&action).map_err(S::Error::from), depth + 1));
+      .map(|action| state.result(&action).map_err(S::Error::from));
 
     self.states.extend(actions);
-    Some(Ok(state))
+    Some(Ok(state.state()))
   }
 }
 
 impl<S: State, D: Decision<S>> Search<S> for Dls<S, D> {
   fn restart_from(&mut self, start: S) -> Result<(), S::Error> {
     self.states.clear();
-    self.states.push((Ok(start), 0));
+    self.states.push(Ok(start.into()));
     Ok(())
   }
 }
