01eab311eeb38f811d98b8704460f9230e2954ce
Alex Colby <alexanderdcolby@gmail.com>
2023-09-10 21:17:04 +0100

    docs: updates doc format on `State`

--- src/state/mod.rs
+++ src/state/mod.rs
@@ -10,36 +10,43 @@ use std::fmt::Debug;
 /// representing edges. We also look to solve the slightly reduced problem of
 /// finding any path to a goal state, rather than the shortest path.
 pub trait State: Sized {
-  /// A supertype for all errors thrown during state generation<br>
+  /// A supertype for all errors thrown during state generation
+  ///
   /// All errors should be convertable into this type
   type Error: Debug + From<Self::ObserveError> + From<Self::ResultError>;
 
-  /// A type for observations of the state<br>
+  /// A type for observations of the state
+  ///
   /// This should be a (potentially) restricted view on the state
   type Observation;
 
   /// The error produced whilst attempting to observe the state
   type ObserveError;
-  /// Returns an observation of the state<br>
+  /// Returns an observation of the state
+  ///
   /// This should be used with to determine the next action to take on the state
   fn observe(&self) -> Result<Self::Observation, Self::ObserveError>;
 
-  /// A type for actions that can be taken on this state<br>
+  /// A type for actions that can be taken on this state
+  ///
   /// We should be able to transition from this state with anything satisfying
   /// the action type
   type Action;
 
-  /// A type for collections of Actions for this state<br>
+  /// A type for collections of Actions for this state
+  ///
   /// This represents all valid Actions that can be taken on this state
   type ActionIter: IntoIterator<Item = Self::Action>;
 
-  /// Returns all valid actions that can be taken on this state<br>
+  /// Returns all valid actions that can be taken on this state
+  ///
   /// This should be used with `observe` to determine the next action to take
   fn actions(&self) -> Self::ActionIter;
 
   /// The error produced whilst attempting to transition to a new state
   type ResultError;
-  /// Takes an action and transitions into a new state<br>
+  /// Takes an action and transitions into a new state
+  ///
   /// This should produce a new state from a reference to this state (i.e. via
   /// `clone`)
   fn result(&self, action: &Self::Action) -> Result<Self, Self::ResultError>;
