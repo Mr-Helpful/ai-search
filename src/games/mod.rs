f15b58c93c1af1220c6ffdc4c9bfb9598ff26212
Alex Colby <alexanderdcolby@gmail.com>
2023-10-07 17:00:49 +0100

    feat: implements MVP for alpha beta pruning

--- src/games/mod.rs
+++ src/games/mod.rs
@@ -1,6 +1,8 @@
-//! @done Implement MVP 2-Player minimax.<br>
-//! @done Modify for N-Player minimax.<br>
-//! @todo Add Alpha-Beta pruning.<br>
+// @done Implement MVP 2-Player minimax.<br>
+// @done Modify for N-Player minimax.<br>
+// @todo Add Alpha-Beta pruning.<br>
 
+mod alphabeta;
+pub use alphabeta::AlphaBeta;
 mod minimax;
 pub use minimax::MiniMax;
